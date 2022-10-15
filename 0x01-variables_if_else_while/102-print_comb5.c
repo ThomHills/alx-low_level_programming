10ce2bab515b8b53a9d896e4ab84f5e4a9e3c941
Thom <thomphiri9@gmail.com>
2022-10-14 17:15:30 -0700

    Updates

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,43 +10,34 @@
  */
 
 int main(void)
-
 {
+	int n1 = 48;
 
-int n1 = 48;
+	int a = 0;
 
-int a = 0;
+	int b;
 
-int b;
+	int com = 44;
 
-int com = 44;
-
-while (a <= 99)
-
-{
-
-b = a + 1;
-
-while (b <= 99)
-
-{
-
-putchar((a / 10) + n1);
-putchar((a % 10) + n1);	
-putchar(32);
-putchar((b / 10) + n1);	
-putchar((b % 10) + n1);
-
-if (a != 98 || b != 99); 
-
-putchar(com); 
-putchar(32); 
-}
-b += 1;
-a += 1;
-putchar('\n');
-
-return (0);
-
-}
+	while (a <= 99)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			putchar((a / 10) + n1);
+			putchar((a % 10) + n1);
+			putchar(32);
+			putchar((b / 10) + n1);
+			putchar((b % 10) + n1);
+			if (a != 98 || b != 99);
+			{
+				putchar(com);
+				putchar(32);
+			}
+			b += 1;
+		}
+		a += 1;
+	}
+	putchar('\n');
+	return (0);
 }
